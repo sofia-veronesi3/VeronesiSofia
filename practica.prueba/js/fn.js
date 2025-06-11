1da916a9848af5f3745f1d5b89ede084f262ef5f
sofia-veronesi3 <veronesisofia2003@gmail.com>
2025-06-11 18:04:35 -0300

    Revert "Trabajo Practico entregable"
    This reverts commit 75bf164db1e526833302b80e2403266728151b05.

--- practica.prueba/js/fn.js
+++
@@ -1,5 +0,0 @@
-
-        function cambiarTexto() {
-            let parrafo = document.getElementById("parrafo-interactivo");
-            parrafo.innerHTML = "¡El texto ha cambiado gracias a JavaScript!"; // Cambia el contenido del párrafo
-        }
--- practica.prueba/js/fn3..js
+++
@@ -1,7 +0,0 @@
-
-let nuevoTexto= document.getElementById('texto');
-
-function cambiarTexto(){
-    nuevoTexto.innerHTML='texto cambiado sabiendo utilizar el DOM, desde java script';
-
-};
